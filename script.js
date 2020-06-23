503ed06056db6fbe67c45085edc5c122fa310ef8
Wilson Birch <wilsonbirch@gmail.com>
2020-06-23 15:00:11 -0400

    sucessfully implemented both ajax calls, begin writing correct numbers to the html file

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 
 
-      // Performing an AJAX request with the queryURL
+      // Performing an AJAX request with the queryURL to receive the current weather report
       $.ajax({
-        url: "api.openweathermap.org/data/2.5/weather?q=Ottawa&api_key=07271f26d162d3364a0ceebb18e7b9eb",
+        url: "http://api.openweathermap.org/data/2.5/weather?q=Toronto&appid=be3ec8db9db08c35a36bef393013c2cb",
+        method: "GET"
+      }).then(function(response) {
+          console.log(response);
+          $("#currentCityName").text("Toronto");
+          var tempC = response.main.temp - 273.15;
+          tempC = tempC.toFixed(1);
+          $("#currentCityTemp").text("Temperature: "+ tempC +" °C");
+
+      })
+
+      // Performing an AJAX request with the queryURL to receive the 5 day forecast report
+      $.ajax({
+        url: "http://api.openweathermap.org/data/2.5/forecast?q=Toronto&appid=be3ec8db9db08c35a36bef393013c2cb",
         method: "GET"
       }).then(function(response) {
           console.log(response);
